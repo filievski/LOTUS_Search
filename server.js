cfffcd74e9f4580dcda793ef6543b9fc4f28bf76
filipdbrsk <filip.dbrsk@gmail.com>
2015-06-21 19:34:03 +0200

    sparql endpoint for identities

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express();
 var async = require('async');
 var request = require('request');
+var SparqlClient = require('sparql-client');
+var util = require('util');
 
 function get_candidate_strings(q, callback){
 	data = {"query": { "match": { "addr": q}}};	
@@ -38,6 +40,29 @@ function get_fuzzy_candidate_strings(q, callback){
         });
 }
 
+function get_identical_resources(q, callback){
+/*
+	query = encodeURIComponent("SELECT * WHERE { GRAPH ?g { ?hello owl:sameAs ?res } } LIMIT 5");
+        data = {"query": query};
+        request({url: 'http://localhost:8890/sparql', method: 'POST', json: true, headers: { "accept": "application/json" }, body: JSON.stringify(data)}, function(error, response, body) {
+                if (!error && response.statusCode == 200)
+                {
+                        console.log(body);
+                        callback(body);
+                } else{
+                        console.log("ERROR" + error);
+                }
+        });
+
+*/
+	var endpoint = 'http://localhost:8890/sparql';
+        var query = "SELECT ?res WHERE { { <" + q + "> owl:sameAs ?res } UNION { ?res owl:sameAs <" + q + "> } }";
+	var client = new SparqlClient(endpoint);
+	client.query(query).execute({format: 'resource', resource: 'res'}, function(error, results) {
+		callback(results["results"]["bindings"]);
+	});
+}
+
 function query_anytime(word, func){
 	func(word + "!");
 }
@@ -55,6 +80,12 @@ app.get('/candidates', function(req, res){
 app.get('/fuzzycandidates', function(req, res){
         get_fuzzy_candidate_strings(req.param('query'), function(cands){
                 res.send(cands);
+        });
+});
+
+app.get('/identical', function(req, res){
+        get_identical_resources(req.param('query'), function(resources){
+                res.send(resources);
         });
 });
 
