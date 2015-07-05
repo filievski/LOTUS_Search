9907e7f8256f454ea0479ddc5966dda2b5a2b16a
Filip Ilievski <filip.dbrsk@gmail.com>
2015-07-05 16:04:50 +0200

    rounding up monuments evaluation

--- examples/ask_for_it.py
+++ examples/ask_for_it.py
@@ -11,7 +11,7 @@ import csv
 import json
 
 def lookup_literal(literal, language):
-	url="http://textindex.fii800.d2s.labs.vu.nl/candidates?" + urllib.urlencode({"query": literal, "size": 100})
+	url="http://lotus.fii800.d2s.labs.vu.nl/candidates?" + urllib.urlencode({"query": literal, "size": 100})
 	print url
 	raw_content = urllib2.urlopen(url).read()
 	content=json.loads(raw_content)
@@ -20,10 +20,15 @@ def lookup_literal(literal, language):
 	all_hits=[]
 	total=0.0
 	dbpedia=0.0
+	global total_dbpedia
+	global total_total
+	global total_took
 	for hit in content["hits"]["hits"]:
 		if "dbpedia.org/resource" in hit["_source"]["subject"]:
 			dbpedia+=1.0
+			total_dbpedia+=1.0
 		total+=1.0
+		total_total+=1.0
 		if total<=20.0:
 			all_hits.append(hit["_source"]["subject"])
 	try:
@@ -31,7 +36,12 @@ def lookup_literal(literal, language):
 	except:
 		dbp_share=None
 	print took
+	total_took+=took
 	return took, num_hits, dbp_share, all_hits
+
+total_dbpedia=0.0
+total_total=0.0
+total_took=0.0
 
 if __name__ == '__main__':
 
@@ -54,4 +64,6 @@ if __name__ == '__main__':
 						time, num_hits, dbp_share, all_hits = lookup_literal(row[0], "nl")
 						spamwriter.writerow([row[0], row[1], row[2], row[3], time, num_hits, dbp_share, all_hits])
 					else: # CONLL or COLD conferences
-						lookup_literal(row[0], "en")
+						time, num_hits, dbp_share, all_hits = lookup_literal(row[0], "en")
+						spamwriter.writerow([row[0], row[1], time, num_hits, dbp_share, all_hits])
+	print total_dbpedia, total_total, total_took
