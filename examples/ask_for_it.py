e61146f334e16ecdeb183f59eafe82192e7c8d24
Filip Ilievski <filip.dbrsk@gmail.com>
2015-07-05 18:14:57 +0200

    running aida

--- examples/ask_for_it.py
+++ examples/ask_for_it.py
@@ -52,18 +52,19 @@ if __name__ == '__main__':
 	path=sys.argv[1]
 
 	for file in os.listdir(path):
-		fullpath=path + "/" + file
-		writepath=path + "/out." + file
-		with open(writepath, "wb") as writefile:	
-			spamwriter=csv.writer(writefile, delimiter=',', quotechar='"')
-			spamwriter.writerow(["Literal", "Source", "Part of the text", "Entity type", "ES Time elapsed", "# ES Hits", "DBpedia % (in first 100)", "Hits"])
-			with open(fullpath, 'rb') as csvfile:
-				spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-				for row in spamreader:
-					if path=="monuments":
-						time, num_hits, dbp_share, all_hits = lookup_literal(row[0], "nl")
-						spamwriter.writerow([row[0], row[1], row[2], row[3], time, num_hits, dbp_share, all_hits])
-					else: # CONLL or COLD conferences
-						time, num_hits, dbp_share, all_hits = lookup_literal(row[0], "en")
-						spamwriter.writerow([row[0], row[1], time, num_hits, dbp_share, all_hits])
+		if file.endswith(".csv"):
+			fullpath=path + "/" + file
+			writepath=path + "/out." + file
+			with open(writepath, "wb") as writefile:	
+				spamwriter=csv.writer(writefile, delimiter=',', quotechar='"')
+				spamwriter.writerow(["Literal", "Source", "Part of the text", "Entity type", "ES Time elapsed", "# ES Hits", "DBpedia % (in first 100)", "Hits"])
+				with open(fullpath, 'rb') as csvfile:
+					spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+					for row in spamreader:
+						if path=="monuments":
+							time, num_hits, dbp_share, all_hits = lookup_literal(row[0], "nl")
+							spamwriter.writerow([row[0], row[1], row[2], row[3], time, num_hits, dbp_share, all_hits])
+						else: # CONLL or COLD conferences
+							time, num_hits, dbp_share, all_hits = lookup_literal(row[0], "en")
+							spamwriter.writerow([row[0], row[1], time, num_hits, dbp_share, all_hits])
 	print total_dbpedia, total_total, total_took
