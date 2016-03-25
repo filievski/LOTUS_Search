f0f088e472ebf918aa49a1bd16e80886b856f813
Filip Ilievski <filip.dbrsk@gmail.com>
2016-03-25 19:48:16 +0100

    Query for predicates and subjects. Ignore blank nodes. Documentation updated.

--- server.js
+++ server.js
@@ -13,26 +13,25 @@ var query_url = 'https://' + configuration.auth + '@lotus.lucy.surfsara.nl/lotus
 
 var numeric_ranks = {"degree": "degree", "numdocs": "r2d", "recency": "timestamp", "lengthnorm": "length", "semrichness": "sr", "termrichness": "tr"};
 
-function retrieve(q, langtag, size, matching, ranking, callback){
+function retrieve(q, langtag, size,  matching, ranking, slop, minmatch, cutoff_freq, fuzziness_level, ip, subject, predicate, filterBlankNodes, callback){
         // MATCHING
-        slop=1;
-        minmatch="70%";
-        fuzziness_level=1;
-        cutoff_freq=0.85;
+	fs.appendFile("out.txt", filterBlankNodes + "\n");
         if (matching=="terms"){
-                var mq={ "match": { "string": {"query": q, "minimum_should_match": minmatch}}};
-         } else if (matching=="phrase"){
-                var mq={ "match_phrase": { "string": {"query": q, "slop": slop}}};
+                var mq=[{ "match": { "string": {"query": q, "minimum_should_match": minmatch}}}];
+        } else if (matching=="phrase"){
+                var mq=[{ "match_phrase": { "string": {"query": q, "slop": slop}}}];
         } else if (matching=="conjunct"){
-                var mq = {"common": {"string": {"query": q, "cutoff_frequency": cutoff_freq, "low_freq_operator": "and"}}};
+                var mq = [{"common": {"string": {"query": q, "cutoff_frequency": cutoff_freq, "low_freq_operator": "and"}}}];
         } else { //Fuzzy
-                var mq = { "match": { "string": { "query": q, "fuzziness": fuzziness_level, "operator": "and"} } };
+                var mq = [{ "match": { "string": { "query": q, "fuzziness": fuzziness_level, "operator": "and"} } }];
         }
 
         // RANKING
 
         if (ranking in numeric_ranks){ // Relational ranking
                 key=numeric_ranks[ranking];
+		//if (key=="degree" || key=="r2d")
+		//	query_url='https://' + configuration.auth + '@lotus.lucy.surfsara.nl/lotus22/_search';
                 var data={ "query": { "function_score": { "query": mq , "field_value_factor": { "field": key }, "boost_mode": "replace" } }, "size": size };
         } else { // Content-based ranking
                 if (ranking=="proximity") {
@@ -40,22 +39,37 @@ function retrieve(q, langtag, size, matching, ranking, callback){
                         var data={"query":{"bool":{"must": mq, "should": rq }}, "size": size};
                 }
                 else if (ranking=="psf"){
+			var must=mq;
                         if (langtag!="any"){
-                                var data={"query":{"bool":{"must": [mq, {"term": {"langtag": langtag }}]}}, "size": size};
-                        } else
-                                var data={"query": mq, "size": size};
+				if (langtag!="any") must.push({"term": {"langtag": langtag }});
+				//if (subject!="") must.push({"term": {"subject": subject }});
+				//if (predicate!="") must.push({"term": {"predicate": "http://era.rkbexplorer.com/id/title" }});
+			}
+                        var data={"query": {"bool":{"must": must}}, "size": size};
+                        //} else
+                        //        var data={"query": mq, "size": size};
                 }
         }
-
+	if (subject){
+		data["query"]["bool"]["must"].push({"query_string": {"default_field": "lit.subject", "query": subject}});
+	}
+	if (predicate){
+                data["query"]["bool"]["must"].push({"query_string": {"default_field": "lit.predicate", "query": predicate}});
+        } 
+	if (filterBlankNodes){
+		data["query"]["bool"]["must_not"]= [{"query_string": {"default_field": "lit.subject","query": "http://lodlaundromat.org/.well-known/genid"}}];
+	}
+	
 	// LANGTAG
 //	if (langtag!="any"){
 //		var data={"query":{"bool":{"must": [mq, {"term": {"langtag": langtag }}]}}, "size": size};
 //	} else
 //		var data={"query": mq, "size": size};
-
-	console.log(JSON.stringify(data));
+        fs.appendFile("out.txt", JSON.stringify(data) + "\n");
         request({url: query_url, method: 'POST', json: true, headers: { "content-type": "application/json" }, body: JSON.stringify(data)}, function(error, response, body) {
-                logRequest(error, response.statusCode.toString(), JSON.stringify(data), body["took"], body["hits"]["total"]);
+               
+		console.log(body);
+		logRequest(error, response.statusCode.toString(), ip, JSON.stringify(data), body["took"], body["hits"]["total"]);
                 if (!error && response.statusCode == 200)
                 {
                         callback(body["took"], body["hits"]["total"], body["hits"]["hits"].map(function(o){
@@ -145,8 +159,8 @@ function lookup_fuzzy_terms(q, size, langtag, fuzziness_level, callback){
 	});
 }
 
-var logRequest = function(error, statusCode, reqJson, took, numhits) {
-	fs.appendFile('reqs.txt', new Date().toISOString() + ' | ' +  error + ' | ' + statusCode + ' | ' + reqJson + ' | ' + took + ' | ' + numhits + '\n', function (err){
+var logRequest = function(error, statusCode, ip, reqJson, took, numhits) {
+	fs.appendFile('reqs.txt', new Date().toISOString() + ' | ' +  error + ' | ' + statusCode + ' | ' + ip + '|' + reqJson + ' | ' + took + ' | ' + numhits + '\n', function (err){
 	});
 }
 
@@ -154,6 +168,10 @@ var logRequest = function(error, statusCode, reqJson, took, numhits) {
 
 app.get('/', function(req, res){
     res.sendFile('index.html', {root:'./client'});
+});
+
+app.get('/docs', function(req, res){
+    res.sendFile('docs.html', {root:'./client'});
 });
 
 app.get('/main.js', function(req, res){
@@ -173,13 +191,24 @@ app.get('/cssload.css', function(req, res){
 });
 
 app.get('/retrieve', function(req, res){
-	if (req.param('string') && req.param('match') && req.param('match')){
-		retrieve(req.param('string'), req.param('langtag') || 'any', req.param('size') || 10, req.param('match'), req.param('rank'), function(took, hits, cands){
+	slop=1;
+        minmatch="70%";
+        fuzziness_level=1;
+        cutoff_freq=0.85;
+
+	if (req.param('string')){
+		var ip = req.headers['x-forwarded-for'] || 
+		     req.connection.remoteAddress || 
+		     req.socket.remoteAddress ||
+		     req.connection.socket.remoteAddress;
+		retrieve(req.param('string'), req.param('langtag') || 'any', req.param('size') || 10, req.param('match') || 'phrase', req.param('rank') || 'psf', req.param('slop') || 1, req.param('minmatch') || '70%', req.param('cutoff') || 0.001, req.param('fuzziness') || 1, ip, req.param('subject'), req.param('predicate'), req.param('noblank')=="true", function(took, hits, cands){
 			res.send({"took": took, "numhits": hits, "hits": cands});
 		});
 	}
-	else
-		res.send("Error: not all parameters set!");
+	else{
+		res.status(400)        // HTTP status 404: NotFound
+   		.send('Error: please set the string parameter!');
+	}
 });
 
 
